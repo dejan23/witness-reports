6a88ce16b345a942fea71408adbccb8233f18f9b
Dejan Stojadinovic <dejan.dvte@gmail.com>
2021-02-05 20:18:23 +0100

    Adding catch everything else route

--- routes/reports.js
+++ routes/reports.js
@@ -1,5 +1,11 @@
+const { response } = require("express");
 const Reports = require("../controllers/reports");
 
 module.exports = function (app) {
   app.get("/api/reports", Reports.fetchReports);
+  app.get("/*", (req, res) =>
+    res.send({
+      message: "Route does not exist, try http://localhost:3000/api/reports",
+    })
+  );
 };
