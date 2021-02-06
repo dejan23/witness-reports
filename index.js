6c09fb57582b985130078744fb17a4caee853371
Dejan Stojadinovic <dejan.dvte@gmail.com>
2021-02-06 10:41:39 +0100

    Removing unnecessary code

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const app = express();
 
 require("./routes/reports")(app);
 
+app.listen(3000, () => {
+  console.log("Listening on http://localhost:3000");
+});
+
 module.exports = app;
-
-const start = () => {
-  app.listen(3000, () => {
-    console.log("Listening on http://localhost:3000");
-  });
-};
-
-start();
